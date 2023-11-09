cedfe5c08799d6f54bd38a1fc007a68bb8dc63a9
NT NGUYÊN <ntnguyen10222002@gmail.com>
2023-11-10 00:31:23 +0700

    #40

--- frontend/src/containers/System/UserManage.js
+++ frontend/src/containers/System/UserManage.js
@@ -3,18 +3,22 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
 import { getAllUsers } from '../../services/userService';
+import { bind } from 'lodash';
+import ModalUser from './ModalUser.js';
+
 class UserManage extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             arrUsers: [],
+            isOpenModalUser: false,
         }
     }
 
     async componentDidMount() {
-        let  response = await getAllUsers('ALL');
-        if(response  && response.errCode===0){
+        let response = await getAllUsers('ALL');
+        if (response && response.errCode === 0) {
             this.setState({
                 arrUsers: response.users
             })
@@ -23,46 +27,69 @@ class UserManage extends Component {
 
     }
 
+    handleAddNewUser = () => {
+        this.setState({
+            isOpenModalUser: true,
+        })
+    }
+
+    toggleUserModal = () => {
+        this.setState({
+            isOpenModalUser: !this.state.isOpenModalUser,
+        })
+    }
 
     render() {
-        let arrUsers= this.state.arrUsers;
+        let arrUsers = this.state.arrUsers;
+        console.log(arrUsers)
         return (
-        <div className="user-container">
-            <div className="text-center">Manage users</div>
-            <div className="users-table mt-3 mx-1" ><table id="customers">
-                <tr>
-                    <th>Email</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Address</th>
-                    <th>Actions</th>
-                </tr>
-                
-                    {arrUsers && arrUsers.map((item, index)=>{
-                        console.log('check map',item , index)
+            <div className="users-container">
+                <ModalUser
+                    isOpen={this.state.isOpenModalUser}
+                    toggleFormParent={this.toggleUserModal}
+                    test={'abc'}
+                />
+                <div className="title text-center">Manage users</div>
+                <div className="mx-1">
+                    <button className="btn btn-primary px-3"
+                        onClick={() => this.handleAddNewUser()}
+                    ><i className="fa fa-plus"></i> Add new users</button>
+                </div>
+
+                <div className="users-table mt-3 mx-1" ><table id="customers">
+                    <tr>
+                        <th>Email</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Address</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    {arrUsers && arrUsers.map((item, index) => {
+                        console.log('check map', item, index)
                         return (
-                        <tr>
-                         
-                            <td>{item.email}</td>
-                            <td>{item.firstName}</td>
-                            <td>{item.lastName}</td>
-                            <td>{item.address}</td>
-                            <td>
-                                <button >Edit</button>
-                                <button >Delete</button>
-                            </td>
-                                
-                            
-                        </tr>
+                            <tr>
+
+                                <td>{item.email}</td>
+                                <td>{item.firstName}</td>
+                                <td>{item.lastName}</td>
+                                <td>{item.address}</td>
+                                <td>
+                                    <button >Edit</button>
+                                    <button >Delete</button>
+                                </td>
+
+
+                            </tr>
                         )
-                    })} 
-                   
-                
-                
+                    })}
+
+
+
                 </table>
+                </div>
+
             </div>
-            
-        </div>
         );
     }
 
