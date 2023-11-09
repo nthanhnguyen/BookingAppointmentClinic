d4989f06e3ea9e719e2e04516db1f7b7edf15730
DucAnh <ducanhnv312@gmail.com>
2023-11-09 20:24:26 +0700

    #47

--- frontend/src/store/actions/appActions.js
+++ frontend/src/store/actions/appActions.js
@@ -8,3 +8,8 @@ export const setContentOfConfirmModal = (contentOfConfirmModal) => ({
     type: actionTypes.SET_CONTENT_OF_CONFIRM_MODAL,
     contentOfConfirmModal: contentOfConfirmModal
 });
+
+export const changeLaunguageApp = (languageInput) => ({
+    type: actionTypes.CHANGE_LANGUAGE,
+    language: languageInput
+})
