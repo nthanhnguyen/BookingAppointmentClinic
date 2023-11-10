eee84cfe306ee41e47ab1f7d95e061f9459fd35a
ChauQuocThai <chauquocthai10@gmail.com>
2023-11-11 02:32:48 +0700

    #51

--- frontend/src/containers/HomePage/HomePage.js
+++ frontend/src/containers/HomePage/HomePage.js
@@ -5,6 +5,8 @@ import Specialty from './Section/Specialty';
 import MedicalFacility from './Section/MedicalFacility';
 import OutStandingDoctor from './Section/OutStandingDoctor';
 import HandBook from './Section/HandBook';
+import About from './Section/About';
+import HomeFooter from './HomeFooter';
 import './HomePage.scss'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -33,6 +35,8 @@ class HomePage extends Component {
                 <HandBook
                     settings = {settings}
                 />
+                <About></About>
+                <HomeFooter />
             </div>
         );
     }
