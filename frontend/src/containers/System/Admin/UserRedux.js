b7b86779656af19958046c378557f0e6ad7b6733
NT NGUYÊN <ntnguyen10222002@gmail.com>
2023-11-11 14:14:11 +0700

    #57

--- frontend/src/containers/System/Admin/UserRedux.js
+++ frontend/src/containers/System/Admin/UserRedux.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import { getAllCodeService } from '../../../services/userService';
 import { LANGUAGES } from '../../../utils';
+import * as actions from '../../../store/actions';
 
 class UserRedux extends Component {
     constructor(props) {
@@ -13,25 +14,35 @@ class UserRedux extends Component {
     }
 
     async componentDidMount() {
-        try {
-            let res = await getAllCodeService('gender');
-            if (res && res.errCode === 0) {
-                this.setState({
-                    genderArr: res.data
-                })
-            }
+        this.props.getGenderStart();
+        //this.props.dispatch(actions.getGenderStart())
+        // try {
+        //     let res = await getAllCodeService('gender');
+        //     if (res && res.errCode === 0) {
+        //         this.setState({
+        //             genderArr: res.data
+        //         })
+        //     }
 
-            console.log('check res: ', res)
-        } catch (e) {
-            console.log(e)
+        //     console.log('check res: ', res)
+        // } catch (e) {
+        //     console.log(e)
+        // }
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.genderRedux !== this.props.genderRedux) {
+            this.setState({
+                genderArr: this.props.genderRedux
+            })
         }
     }
 
 
     render() {
-        console.log("check state: ", this.state)
         let genders = this.state.genderArr;
         let language = this.props.language;
+        console.log("check props from redux: ", this.props.genderRedux)
         return (
             <div className='user-redux-container'>
                 <div className="title" >User Redux</div>
@@ -112,12 +123,14 @@ class UserRedux extends Component {
 
 const mapStateToProps = state => {
     return {
-        language: state.app.language
+        language: state.app.language,
+        genderRedux: state.admin.genders
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        getGenderStart: () => dispatch(actions.fetchGenderStart())
     };
 };
 
--- frontend/src/store/reducers/rootReducer.js
+++ frontend/src/store/reducers/rootReducer.js
@@ -4,6 +4,7 @@ import { connectRouter } from 'connected-react-router';
 import appReducer from "./appReducer";
 
 import userReducer from "./userReducer";
+import adminReducer from './adminReducer';
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
@@ -21,11 +22,12 @@ const userPersistConfig = {
 };
 const appPersistConfig = {
     ...persistCommonConfig,
-    key:'app',
+    key: 'app',
     whitelist: ['language']
 };
 export default (history) => combineReducers({
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
-    app: persistReducer(appPersistConfig, appReducer), 
+    app: persistReducer(appPersistConfig, appReducer),
+    admin: adminReducer
 })
