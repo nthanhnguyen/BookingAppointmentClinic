817ee5bcefb53f8da2535b9b907e880f43eb7054
NT NGUYÊN <ntnguyen10222002@gmail.com>
2023-11-11 10:50:08 +0700

    #56

--- frontend/src/routes/System.js
+++ frontend/src/routes/System.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { Redirect, Route, Switch } from 'react-router-dom';
 import UserManage from '../containers/System/UserManage';
-import UserRedux from '../containers/System/UserRedux';
+import UserRedux from '../containers/System/Admin/UserRedux';
 
 import Header from '../containers/Header/Header';
 
 class System extends Component {
     render() {
 
-        const { systemMenuPath,isLoggedIn } = this.props;
+        const { systemMenuPath, isLoggedIn } = this.props;
         return (
             <React.Fragment>
                 {isLoggedIn && <Header />}
@@ -18,7 +18,7 @@ class System extends Component {
                         <Switch>
                             <Route path="/system/user-manage" component={UserManage} />
                             <Route path="/system/user-redux" component={UserRedux} />
-                            
+
                             <Route component={() => { return (<Redirect to={systemMenuPath} />) }} />
                         </Switch>
                     </div>
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -22,7 +22,13 @@ const deleteUserService = (userId) => {
 const editUserService = (inputData) => {
     return axios.put('api/edit-user', inputData);
 }
+
+const getAllCodeService = (inputType) => {
+    return axios.get(`/api/allcode?type=${inputType}`);
+}
+
 export {
     handleLoginApi, getAllUsers,
-    createNewUserService, deleteUserService, editUserService
+    createNewUserService, deleteUserService,
+    editUserService, getAllCodeService
 };
