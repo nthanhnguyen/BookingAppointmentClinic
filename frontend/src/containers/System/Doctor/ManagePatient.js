704c9fe77d88594562652d954f096a9c3c5f3619
NT NGUYÊN <ntnguyen10222002@gmail.com>
2023-11-25 23:00:38 +0700

    #100 38:20

--- frontend/src/containers/System/Doctor/ManagePatient.js
+++ frontend/src/containers/System/Doctor/ManagePatient.js
@@ -6,13 +6,17 @@ import { FormattedMessage } from 'react-intl';
 import DatePicker from '../../../components/Input/DatePicker';
 import { getAllPatientForDoctor } from '../../../services/userService';
 import moment from 'moment';
+import RemedyModal from './RemedyModal';
+
 class ManagePatient extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             currentDate: moment(new Date()).startOf('day').valueOf(),
-            dataPatient: []
+            dataPatient: [],
+            isOpenRemedyModal: false,
+            dataModal: {}
         }
     }
 
@@ -52,66 +56,96 @@ class ManagePatient extends Component {
             this.getDataPatient(user, formatedDate)
         })
     }
-    handleBtnConfirm = () => {
-
+    handleBtnConfirm = (item) => {
+        let data = {
+            doctorId: item.doctorId,
+            patientId: item.patientId,
+            email: item.patientData.email
+        }
+        this.setState({
+            isOpenRemedyModal: true,
+            dataModal: data
+        })
+        console.log('Check dataaa', data)
     }
-    handleBtnRemedy = () => {
 
+    closeRemedyModal = () => {
+        this.setState({
+            isOpenRemedyModal: false,
+            dataModal: {}
+        })
+    }
+
+    sendRemedy = (dataFromModal) => {
+        console.log('parent check modal: ', dataFromModal)
     }
 
     render() {
-        console.log('Check state from patient', this.state)
-        let { dataPatient } = this.state
+        let { dataPatient, isOpenRemedyModal, dataModal } = this.state;
+        let { language } = this.props
         return (
-            <div className='manage-patient-container'>
-                <div className='m-p-title'>
-                    Quản lý bệnh nhân khám bệnh
-                </div>
-                <div className='manage-patient-body row'>
-                    <div className='col-4 form-group'>
-                        <label>Chọn ngày khám</label>
-                        <DatePicker
-                            onChange={this.handleOnchangeDatePicker}
-                            className="form-control"
-                            value={this.state.currentDate}
-                        />
+            <>
+                <div className='manage-patient-container'>
+                    <div className='m-p-title'>
+                        Quản lý bệnh nhân khám bệnh
                     </div>
-                    <div className='col-12 table-manage-patient'>
-                        <table style={{ width: '100%' }}>
-                            <tbody>
-                                <tr>
-                                    <th>STT</th>
-                                    <th>Thời gian</th>
-                                    <th>Họ và tên</th>
-                                    <th>Địa chỉ</th>
-                                    <th>Giới tính</th>
-                                    <th>Actions</th>
-                                </tr>
-                                {dataPatient && dataPatient.length > 0 && dataPatient.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td>{index + 1}</td>
-                                            <td>{item.timeTypeDataPatient.valueVi}</td>
-                                            <td>{item.patientData.firstName}</td>
-                                            <td>{item.patientData.address}</td>
-                                            <td>{item.patientData.genderData.valueVi}</td>
-                                            <td>
-                                                <button className='mp-btn-confirm'
-                                                    onClick={() => this.handleBtnConfirm()}>Xác nhận</button>
-                                                <button className='mp-btn-remedy'
-                                                    onClick={() => this.handleBtnRemedy()}>Gửi hóa đơn</button>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
+                    <div className='manage-patient-body row'>
+                        <div className='col-4 form-group'>
+                            <label>Chọn ngày khám</label>
+                            <DatePicker
+                                onChange={this.handleOnchangeDatePicker}
+                                className="form-control"
+                                value={this.state.currentDate}
+                            />
+                        </div>
+                        <div className='col-12 table-manage-patient'>
+                            <table style={{ width: '100%' }}>
+                                <tbody>
+                                    <tr>
+                                        <th>STT</th>
+                                        <th>Thời gian</th>
+                                        <th>Họ và tên</th>
+                                        <th>Địa chỉ</th>
+                                        <th>Giới tính</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                    {dataPatient && dataPatient.length > 0 && dataPatient.map((item, index) => {
+                                        let time = language === LANGUAGES.VI ?
+                                            item.timeTypeDataPatient.valueVi : item.timeTypeDataPatient.valueEn;
+                                        let gender = language === LANGUAGES.VI ?
+                                            item.patientData.genderData.valueVi : item.patientData.genderData.valueEn;
+                                        return (
+                                            <tr key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{time}</td>
+                                                <td>{item.patientData.firstName}</td>
+                                                <td>{item.patientData.address}</td>
+                                                <td>{gender}</td>
+                                                {/* <td>{item.patientData.genderData.valueVi}</td> */}
+                                                <td>
+                                                    <button className='mp-btn-confirm'
+                                                        onClick={() => this.handleBtnConfirm(item)}>Xác nhận
+                                                    </button>
 
-                                }
-                            </tbody>
-                        </table>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
 
+                                    }
+                                </tbody>
+                            </table>
+
+                        </div>
                     </div>
                 </div>
-            </div>
+                <RemedyModal
+                    isOpenModal={isOpenRemedyModal}
+                    dataModal={dataModal}
+                    closeRemedyModal={this.closeRemedyModal}
+                    sendRemedy={this.sendRemedy}
+                />
+            </>
         )
 
     }
